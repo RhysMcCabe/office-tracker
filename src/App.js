ad3bfdcb7122bc8b756f7cd337a1e2a46c0045f4
Rhys <rhys.mccabe@hotmail.com>
2023-11-03 02:03:15 +0000

    added stripe

--- src/App.js
+++ src/App.js
@@ -38,56 +38,58 @@ function App() {
     }
   }, [user]);
   return (
-    <div className="bg-zinc-800 h-screen text-center pt-12 text-zinc-200">
-      <div className="">
+    <div className="bg-zinc-800 h-screen text-center pt-12 text-zinc-200 shadow-lg">
+      {user?.email ? (
+        <div className="mb-6">
+          <Logout setUser={setUser} setUserData={setUserData} />
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className=" w-fit mx-auto bg-zinc-900 rounded-2xl p-6">
         <Heading />
-      </div>
-
-      {user === null ? (
-        <div className="flex justify-center">
-          <Login setUser={setUser} />
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {user && user !== "unauthorized" ? (
-        <div>
-          <MainTracker />
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {user === "unauthorized" ? (
-        <div className="text-3xl pt-6 text-red-500">Access Denied</div>
-      ) : (
-        <></>
-      )}
-
-      {userData ? (
-        <div className="text-3xl pt-6 text-red-500">
-          {Object.keys(userData).map((key) => (
-            <div key={key}>
-              {key}:{" "}
-              {Object.keys(userData[key]).map((key2) => (
-                <div key={key2}>{userData[key][key2].toString()}</div>
-              ))}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="mt-64">
-        {" "}
-        {user?.email ? (
-          <div className="">
-            <Logout setUser={setUser} setUserData={setUserData} />
+        {user === null ? (
+          <div className="flex justify-center">
+            <Login setUser={setUser} />
           </div>
         ) : (
           <></>
         )}
+        {user && user !== "unauthorized" ? (
+          <div>
+            <MainTracker />
+          </div>
+        ) : (
+          <></>
+        )}
+        {user === "unauthorized" ? (
+          <div className="text-3xl pt-6 text-red-500">Access Denied</div>
+        ) : (
+          <></>
+        )}
+        {userData ? (
+          <div className="text-sm pt-6 ">
+            {Object.keys(userData).map((key) => (
+              <div key={key}>
+                {key}:{" "}
+                {Object.keys(userData[key]).map((key2) => (
+                  <span key={key2}>{userData[key][key2].toString()}</span>
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}{" "}
+        <div className="mt-20  w-fit mx-auto rounded-2xl bg-zinc-900">
+          <stripe-buy-button
+            buy-button-id="buy_btn_1O8CR9R1MgaUczlogYpaFDX8"
+            publishable-key="pk_test_51MoDkyR1MgaUczloCxubf6C6EJvd844xu2J7mibPXVo4kVUxRLo7RWAFJVmugVRmaVN0yYqmoUiKwFowi61Lnz9D00faQZUP2D"
+          ></stripe-buy-button>
+          <div className="text-zinc-300 text-sm font-semibold">
+            toward server costs
+          </div>
+        </div>
       </div>
     </div>
   );
